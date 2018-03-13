35551690da274cc038a8b3fb88b550272d1c95ea
vijisEEE <36723491+vijisEEE@users.noreply.github.com>
2018-03-13 16:26:17 +0530

    Create oddnum.c

---
+++ oddnum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+for(i=a;i<b;i++)
+{
+if(i%2!=0)
+{
+printf("Odd number: ",i);
+}
+else
+{
+}
+}
+getch();
+}
+
